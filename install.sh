c7498a7affc2a10303cfe09d6abf70d07329e229
alex <alex.suspicious@gmail.com>
2024-05-15 14:54:53 +0500

    new files

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip3 install -U "g4f[all]" &&
+pip3 install rich &&
+wget "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/640px-PNG_transparency_demonstration_1.png" &&
+sudo cp please /bin/please
